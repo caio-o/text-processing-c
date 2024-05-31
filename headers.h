7ebc0fd6fb4ca1746045b17b29600e72c33c92fa
caio-o <caiusbarbieri@gmail.com>
2024-05-31 16:10:34 -0300

    Declarar leArquivo() e imprimeParte()

---
+++ headers.h
@@ -0,0 +1,12 @@
+/*************************************************************/
+/**   HEADER que declara todas as funcoes do programa.       */
+/*************************************************************/
+
+#ifndef __HEADERS_H
+#define __HEADERS_H
+
+void imprimeParte(char *buff, int i, int j);
+
+int leArquivo (char *nome, char **buffer);
+
+#endif
