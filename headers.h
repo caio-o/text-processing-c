76f59719220a2dfc677792d9d743c4184da89909
caio-o <caiusbarbieri@gmail.com>
2024-06-11 14:59:31 -0300

    Cabecalhos das funcoes utilitarias

--- headers.h
+++ headers.h
@@ -7,9 +7,13 @@
 
 void imprimeParte(const char *buff, int i, int j);
 
+void imprimeSetinha(int pos);
+
 int leArquivo (char *nome, char **buffer);
 
 int letraOuNumero(char c);
+
+int menorDoPar (int a, int b);
 
 char* extraiPalavra(char *buffer, int i, int j);
 
