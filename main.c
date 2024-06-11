d514f83c93709486d7f3f097d7066efc5603fa94
caio-o <caiusbarbieri@gmail.com>
2024-06-11 14:58:48 -0300

    Limpar main.c

--- main.c
+++ main.c
@@ -1,34 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "headers.h"
-#include "textoInvertido.h"
-#define FILE_NAME "Historia.txt"
+#include "menu.h"
 
 int main (void)
 {
-	int tam;
-	//struct vocabulo *teste;
-	char *buffer;
-	tam = leArquivo(FILE_NAME, &buffer);
+	menuPrincipal();
 	
-	textoInv *txt = criaTextoInv(buffer, tam);
-
-	if (txt == NULL) 
-	{
-		return 1;
-	}
-
-	imprimeTextoInv(txt);
-	salvaTextoInv(txt, "arquivo-invertido.txt");
-
-	//teste = busca("beto", txt);
-	
-	printf("\n\n\n\n        BUSCA SEQUENCIAL DE quem: \n");
-	//if (teste != NULL) imprimeVocabulo(teste);
-	//else printf("\"beto\" NAO ENCONTRADO!\n");
-	buscaImprime("quem", txt, buffer);
-
-	destroiTextoInv (txt);
-
 	return 0;
 }
