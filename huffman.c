95deb402d6524b9af6af3d89633a093753313d9b
Caio Barbieri <89612905+caio-o@users.noreply.github.com>
2024-06-11 17:20:04 -0300

    Sketch huffman.c

--- huffman.c
+++ huffman.c
@@ -1,5 +1,71 @@
-#include "textoInverido.h"
+#include "textoInvertido.h"
+#include "headers.h"
 
+//colocar isso em textoInvertido.c
+int wordFrequencyFromString (const char *str, textoInv *txt)
+{
+        int ret = 0;
+        struct vocabulo *v = busca(str, txt);
+
+        if (v != NULL)
+        {
+                ret++;
+                struct ocorrencia *at = v->ocorrencias->head;
+
+                while(at->prox != NULL)
+                {
+                        at = at->prox;
+                        ret++;
+                }
+        }
+
+        return ret;
+}
+
+int wordFreqFromNode (struct vocabulo *v)
+{
+        int ret = 1;
+
+        struct ocorrencia *at = v->ocorrencias->head;
+
+        while(at->prox != NULL)
+        {                                                                                                                           
+                at = at->prox;                                                                                                      
+                ret++;
+        }
+
+        return ret;
+}
+
+/**
+NAO ADICIONAR 0, PARA GUARDAR ATE 2
+ADICIONAR 1   0, PARA GUARDAR ATE 4
+ADICIONAR 2   0, PARA GUARDAR ATE 8
+ETC.
+
+int lim = 2;
+int x = 0; //casas a esquerda
+while (PARIDADE > lim)
+{
+        lim *= 2;
+
+        x++;
+}
+
+PARIDADE 1 --> NAO ADICIONAR
+PARIDADE 2 --> NAO ADICIONAR
+PARIDADE 3 --> ADIICONAR 1 CASA
+PARIDADE 4 --> ADICIONAR 1 CASA
+PARIDADE 5 --> ADICIONAR 2 CASAS
+PARIDADE 6 --> ADICIONAR 2 CASAS
+
+
+rosa |  0 | 0 4 12 17 50
+para | 100 | 24 43 55
+cada | 101 | 12 22 33
+uma  | 110 |
+
+*/
 /** COMPRESSAO */
 
 // RAZAO DE COMPRESSAO: TAM_COMP / TAM_ORIG
